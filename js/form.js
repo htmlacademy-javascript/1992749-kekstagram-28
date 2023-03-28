4368ff589792acb2d7d2aa6f950f21dd7a8c20aa
LevDiakov <381208dl@gmail.com>
2023-03-28 12:38:27 -0300

    Правки оформления

--- js/form.js
+++ js/form.js
@@ -9,8 +9,6 @@ const PATTERN = /^#[a-zа-яё0-9]{1,19}$/i;
 const TAG_ERROR = 'Невалидный хэш-тег';
 const hashtagsInputField = document.querySelector('.text__hashtags');
 const commentInputField = document.querySelector('.text__description');
-commentInputField.dataset.pristineMaxlength = '140';
-commentInputField.dataset.pristineMaxlengthMessage = 'Не более 140 символов';
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
@@ -18,65 +16,68 @@ const pristine = new Pristine(form, {
   errorTextClass: 'img-upload__field-wrapper__error-text',
 });
 
-const commentEscape = (evt) => {
+const checkCommentEscape = (evt) => {
   if (isEscapeKey(evt)) {
     evt.stopPropagation();
   }
 };
 
+const assistsСlosing = () => {
+  form.reset();
+  modalWindow.classList.add('hidden');
+  body.classList.remove('modal-open');
+  hashtagsInputField.removeEventListener('keydown', checkCommentEscape);
+  commentInputField.removeEventListener('keydown', checkCommentEscape);
+};
+
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
-    form.reset();
-    modalWindow.classList.add('hidden');
-    body.classList.remove('modal-open');
+    assistsСlosing();
     document.removeEventListener('keydown', onDocumentKeydown);
-    hashtagsInputField.removeEventListener('keydown', commentEscape);
-    commentInputField.removeEventListener('keydown', commentEscape);
   }
 };
 const closeModalWindow = () => {
   pristine.reset();
-  form.reset();
-  modalWindow.classList.add('hidden');
-  body.classList.remove('modal-open');
+  assistsСlosing();
   document.removeEventListener('keydown', onDocumentKeydown);
-  hashtagsInputField.removeEventListener('keydown', commentEscape);
-  commentInputField.removeEventListener('keydown', commentEscape);
 };
 
 const openModalWindow = () => {
   modalWindow.classList.remove('hidden');
   body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
-  hashtagsInputField.addEventListener('keydown', commentEscape);
-  commentInputField.addEventListener('keydown', commentEscape);
+  hashtagsInputField.addEventListener('keydown', checkCommentEscape);
+  commentInputField.addEventListener('keydown', checkCommentEscape);
 };
 
-const checkValidTag = (tag) => PATTERN.test(tag);
-const checkValidCount = (tags) => tags.length <= MAX_TAG_COUNT;
+const initValidation = () => {
+  const checkValidTag = (tag) => PATTERN.test(tag);
+  const checkValidCount = (tags) => tags.length <= MAX_TAG_COUNT;
 
-const checkUniqueTags = (tags) => {
-  const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
-  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+  const checkUniqueTags = (tags) => {
+    const lowerCaseTags = tags.map((tag) => tag.toLowerCase());
+    return lowerCaseTags.length === new Set(lowerCaseTags).size;
+  };
+
+  const validateTags = (value) => {
+    const tags = value
+      .trim()
+      .split(' ')
+      .filter((tag) => tag.trim().length);
+    return checkValidCount(tags) && checkUniqueTags(tags) && tags.every(checkValidTag);
+  };
+
+  pristine.addValidator(
+    hashtagsInputField,
+    validateTags,
+    TAG_ERROR
+  );
 };
-
-const validateTags = (value) => {
-  const tags = value
-    .trim()
-    .split(' ')
-    .filter((tag) => tag.trim().length);
-  return checkValidCount(tags) && checkUniqueTags(tags) && tags.every(checkValidTag);
-};
-
-pristine.addValidator(
-  hashtagsInputField,
-  validateTags,
-  TAG_ERROR
-);
 
 export const initModal = () => {
   fileField.addEventListener('change', openModalWindow);
   closeButton.addEventListener('click', closeModalWindow);
+  initValidation();
   form.addEventListener('submit', (evt) => {
     evt.preventDefault();
     pristine.validate();
