63ed0cef039321e4b75ba1faba9c2f5483bd106d
LevDiakov <381208dl@gmail.com>
2023-04-07 10:14:19 -0300

    Исправление ошибок

--- js/utils.js
+++ js/utils.js
@@ -1,23 +1,13 @@
 const ALERT_SHOW_TIME = 5000;
+const alertContainer = document.querySelector('.show_alert');
 
 export const showAlert = (message) => {
-  const alertContainer = document.createElement('div');
-  alertContainer.style.zIndex = '100';
-  alertContainer.style.position = 'absolute';
-  alertContainer.style.left = '0';
-  alertContainer.style.top = '0';
-  alertContainer.style.right = '0';
-  alertContainer.style.padding = '10px 3px';
-  alertContainer.style.fontSize = '30px';
-  alertContainer.style.textAlign = 'center';
-  alertContainer.style.backgroundColor = 'red';
 
+  alertContainer.classList.remove('hidden');
   alertContainer.textContent = message;
 
-  document.body.append(alertContainer);
-
   setTimeout(() => {
-    alertContainer.remove();
+    alertContainer.classList.add('hidden');
   }, ALERT_SHOW_TIME);
 };
 
